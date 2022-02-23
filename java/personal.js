460752c0818b87c3525ec4a6bab2f8adfb5f16df
JordanG23 <greenwoodjordan@hotmail.com>
2022-02-22 18:34:47 -0700

    updates

--- java/personal.js
+++ java/personal.js
@@ -1,6 +1,6 @@
 var string = document.lastModified;
 
-
 let nLastModif = Date.parse(document.lastModified);
 
 document.body.appendChild(document.createElement("center")).innerText = "Last Updated: " + document.lastModified
+
