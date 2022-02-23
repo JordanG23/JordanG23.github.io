b14e41e2e5e629841e51b79f4a58c7ce58c4214c
JordanG23 <greenwoodjordan@hotmail.com>
2022-02-22 21:57:02 -0700

    updated the copyright date with javascript

--- java/personal.js
+++ java/personal.js
@@ -2,5 +2,6 @@ var string = document.lastModified;
 
 let nLastModif = Date.parse(document.lastModified);
 
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
 document.body.appendChild(document.createElement("center")).innerText = "Last Updated: " + document.lastModified
 
