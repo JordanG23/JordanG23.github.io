6328eedec46b58a7a831beedd3ac861440f60415
JordanG23 <greenwoodjordan@hotmail.com>
2022-03-23 17:29:21 -0700

    added home page

---
+++ lesson-5/js/home.js
@@ -0,0 +1,46 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'].filter((town) => {
+            if(town.name === "Preston" || town.name === "Fish Haven" || town.name === "Soda Springs") {
+                return town;
+            }
+        });
+        for (let i = 0; i < towns.length; i++ ) {
+            let townstat = document.createElement('section');
+            
+            let textfield = document.createElement('article');
+
+            let headline = document.createElement('h2');
+            headline.textContent = towns[i].name;
+            townstat.appendChild(headline);
+
+            let motto = document.createElement('h3');
+            motto.textContent = towns[i].motto;
+            townstat.appendChild(motto);
+
+            let founded = document.createElement('h4');
+            founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            townstat.appendChild(founded);
+
+            let popu = document.createElement('h4');
+            popu.textContent = 'Population: ' + towns[i].currentPopulation;
+            townstat.appendChild(popu);
+
+            let rain = document.createElement('h4');
+            rain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + ' in.';
+            townstat.appendChild(rain);          
+
+            let img = document.createElement('img');
+            img.setAttribute('src', './images/city' + (i + 1) + ".JPG");
+            img.setAttribute('alt', towns[i].name);
+            townstat.appendChild(img);
+
+            document.querySelector('div.townstat').appendChild(townstat);
+        }
+      });
