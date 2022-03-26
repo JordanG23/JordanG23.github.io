fb7f9aedcab0ff03f8f04aca10326b97c3eb3a48
JordanG23 <greenwoodjordan@hotmail.com>
2022-03-26 16:47:09 -0700

    added updated weather api to preston

--- weather/js/windchill.js
+++ weather/js/windchill.js
@@ -1,7 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    var speed = document.getElementById('speed').innerText;
-    var temp = document.getElementById('temp').innerText;
-    let chill = document.getElementById('chill');
+    var speed = document.getElementById("speed").innerText;
+    var temp = document.getElementById("temp").innerText;
+    let chill = document.getElementById("chill");
     chill.innerHTML = buildWC(speed, temp);
 });
 
@@ -11,11 +12,11 @@ function buildWC(speed, temp) {
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
 
-    wc = Math.floor(wc);
+    let floorwc = Math.floor(wc);
 
-    wc = (wc > temp) ? temp : wc;
+    floorwc = (floorwc > temp) ? temp : floorwc;
 
     console.log(wc);
     // chill.innerHTML = wc;
-    return wc;
+    return floorwc;
 }
