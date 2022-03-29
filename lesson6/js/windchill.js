17da272ae9894dd0c3abd6ec136bf7b6b3d451f3
JordanG23 <greenwoodjordan@hotmail.com>
2022-03-28 18:10:48 -0700

    adjustments to windchill js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,9 +2,9 @@
 var temp = document.getElementById('temp').innerText;
 var speed = document.getElementById('speed').innerText;
 
-if (temp <= 50 && speed >= 3) {
+if ( temp <= 50 && speed >= 3) {
     var chill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-    document.querySelector('#chill').innerHTML = chill;
+    document.querySelector('#chill').innerHTML = Math.round(chill);
 }
 else {
     document.querySelector('#chill').innerText = 'N/A';
